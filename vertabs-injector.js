1d676b89da6b19b1ec94cd122d82959e24aaa941
Anton Niklasson <niklasson.anton@gmail.com>
2012-10-14 15:38:57 +0200

    Styling

--- vertabs-injector.js
+++ vertabs-injector.js
@@ -5,16 +5,16 @@ chrome.extension.sendMessage({tabReady: true}, function(response) {
 	var vertabNode = document.createElement("div");
 	vertabNode.setAttribute("id", "vertab");
 
-	var h1Node = document.createElement("h1");
-	h1Node.appendChild(document.createTextNode("Vertabs"));
-	vertabNode.appendChild(h1Node);
+	// var h1Node = document.createElement("h1");
+	// h1Node.appendChild(document.createTextNode("Vertabs"));
+	// vertabNode.appendChild(h1Node);
 
 	var ulNode = document.createElement("ul");
 
 
 	tabs.forEach(function(tab){
 		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(tab.url));
+		li.appendChild(document.createTextNode(tab.title));
 		ulNode.appendChild(li);
 	});
 
