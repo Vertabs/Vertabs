e3121c4dc4cd2634f221f9edc04749aa4da5e161
Anton Niklasson <niklasson.anton@gmail.com>
2012-10-16 21:59:25 +0200

    Cleaned up some more

--- vertabs-injector.js
+++ vertabs-injector.js
@@ -1,35 +1,29 @@
 chrome.extension.onMessage.addListener(
 	function(request, sender, sendResponse) {
-
 		var tabs = request.tabs;
 		var vertabsNode;
 		var ulNode;
 
 		// Remove #vertabs if exists
 		if(!(vertabsNode = document.getElementById("vertabs"))) {
-			// vertabsNode.parentNode.removeChild(vertabsNode);
 			vertabsNode = document.createElement("div");
 			vertabsNode.setAttribute("id", "vertabs");
-
 			ulNode = document.createElement("ul");
 		} else {
 			ulNode = vertabsNode.getElementsByTagName("ul")[0];
 			ulNode.innerHTML = "";
 		}
 
+		// Handles all click events. Delegation.
 		vertabsNode.addEventListener("click", vertabsNodeClickHandler);
 
 		tabs.forEach(function(tab){
 			var li = document.createElement("li");
-			li.setAttribute("data-tab-id", tab.id);
-
-			// This should be delegated to UL!
-			// li.addEventListener("click", switchTab);
-			
+			li.setAttribute("data-tab-id", tab.id);			
 
 			/*
-			Chrome Extensions don't have proper access to favicon stored under chrome://
-			This will only output favicons with normal urls. Might change in the future.£
+			This will only output favicons with normal urls.
+			SO question: http://tinyurl.com/d857xwk
 			*/
 			if(tab.favIconUrl && tab.favIconUrl.indexOf('chrome://') == -1) {
 				var faviconNode = document.createElement("img");
@@ -38,29 +32,25 @@ chrome.extension.onMessage.addListener(
 			}
 
 			var closeNode = document.createElement("img");
-			var closeSrc = chrome.extension.getURL("imgs/close.png");
-			closeNode.setAttribute('src', closeSrc);
+			closeNode.setAttribute('src', chrome.extension.getURL("imgs/close.png"));
 			closeNode.setAttribute('class', 'vertabs-close-icon')
-
-			// This should be delegated to UL!
-			// closeNode.addEventListener("click", function(){console.log("Close!");});
-
 			li.appendChild(closeNode);
 
+			// Title won't be longer than 30
 			var shorttitle = (tab.title.length > 30) ? tab.title.substring(0,27)+"..." : tab.title;
 			li.appendChild(document.createTextNode(shorttitle));
 			
+			// URLs won't be longer than 50
 			smallNode = document.createElement("small");
 			var shorturl = (tab.url.length > 50) ? tab.url.substring(0,47)+"..." : tab.url;
-			urlText = document.createTextNode(shorturl);
-			smallNode.appendChild(urlText);
+			smallNode.appendChild(document.createTextNode(shorturl));
 			li.appendChild(smallNode);
-
 
 			ulNode.appendChild(li);
 		});
 
 		vertabsNode.appendChild(ulNode);
+
 
 		document.body.appendChild(vertabsNode);
 	}
@@ -70,33 +60,25 @@ chrome.extension.onMessage.addListener(
 function vertabsNodeClickHandler(e) {
 	var closeID, tabID;
 
-	console.log(e);
-
-	// Check if close icon was clicked
+	// Close tab if the close icon was clicked
 	if(e.target.className == "vertabs-close-icon") {
-		
 		closeID = e.target.parentNode.getAttribute("data-tab-id");
 		closeTab(closeID);
 
+	// Otherwise switch to clicked tab. Make sure to grab the tab id from the li element
 	} else {
-		
 		if(e.target.nodeName == "IMG" || e.target.nodeName == "SMALL")
 			tabID = e.target.parentNode.getAttribute("data-tab-id");
 		else
 			tabID = e.target.getAttribute("data-tab-id");
 
 		switchTab(tabID);
-
 	}
 }
 
 function switchTab(tabID) {
 	chrome.extension.sendMessage({gotoTab: tabID});
 }
-
 function closeTab(tabID) {
 	chrome.extension.sendMessage({closeTab: tabID});
 }
-
-
-
