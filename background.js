e2447bc83e8fba88f02df8836b8688a974713671
Anton Niklasson <niklasson.anton@gmail.com>
2012-10-16 21:52:43 +0200

    Cleaned up a bit

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
+/*
+Receive message sent from a content script.
+Will switch tab, or close a tab, depending on
+what's specified in the request object.
+*/
+chrome.extension.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if(request.gotoTab) {
+			chrome.tabs.update(parseInt(request.gotoTab), {active:true});
+		} else if(request.closeTab) {
+			chrome.tabs.remove(parseInt(request.closeTab));
+		}
+	}
+);
+
+
+chrome.tabs.onRemoved.addListener(function(tabID, removeInfo){
+	sendTabs();
+});
+chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
+	sendTabs();
+});
+
+
 function sendTabs() {
 	// Fetch all tabs, async
 	chrome.tabs.getAllInWindow(function(tabs){
@@ -10,45 +34,3 @@ function sendTabs() {
 		return true;
 	});
 }
-
-
-chrome.browserAction.onClicked.addListener(function(tab){
-	
-});
-
-
-// Receive message and switch tab
-chrome.extension.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if(request.gotoTab) {
-			chrome.tabs.update(parseInt(request.gotoTab), {active:true});
-		} else if(request.closeTab) {
-			chrome.tabs.remove(parseInt(request.closeTab));
-		}
-	}
-);
-
-
-// onRemoved will update the tablist
-chrome.tabs.onRemoved.addListener(function(tabID, removeInfo){
-	console.log("onRemoved ID: "+tabID);
-	console.log(removeInfo);
-	console.log("");
-
-	sendTabs();
-});
-
-
-chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
-	console.log("onUpdated. Status: "+changeInfo.status);
-	console.log(tab);
-	console.log("");
-
-	sendTabs();
-});
-/*chrome.tabs.onCreated.addListener(function(){
-	console.log("onCreated");
-});*/
-/*chrome.tabs.onActiveChanged.addListener(function(){
-	console.log("onActiveChanged");
-});*/
