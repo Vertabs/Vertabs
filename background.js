4ee7973f0ab507fb97011a6f117d34c2132374f8
Anton Niklasson <niklasson.anton@gmail.com>
2012-10-17 22:35:32 +0200

    Fix

--- background.js
+++ background.js
@@ -31,11 +31,10 @@ chrome.extension.onMessage.addListener(
 Listen for tab events being removed or created.
 */
 chrome.tabs.onRemoved.addListener(function(tabID, removeInfo){
-	if(vertabsActive)
-		sendTabs();
+	sendTabs();
 });
 chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
-	if(vertabsActive)
+	if(vertabsActive[tab.windowId])
 		sendTabs();
 });
 
